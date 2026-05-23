5bacb7901437b35a77cb6e32c3a7e9f13b4eda26
Inge Jørgensen <inge@elektronaut.no>
2026-05-23 20:55:24 +0200

    Update Sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-require "active_support/parameter_filter"
-
 if Rails.application.credentials.sentry_dsn.present? && !Rails.env.test?
   Sentry.init do |config|
     config.dsn = Rails.application.credentials.sentry_dsn
+    config.send_default_pii = true
     config.enabled_environments = %w[staging production]
     config.excluded_exceptions += [
       "ActionDispatch::RemoteIp::IpSpoofAttackError",
@@ -12,14 +11,6 @@ if Rails.application.credentials.sentry_dsn.present? && !Rails.env.test?
       "DynamicImage::Errors::InvalidSignature",
       "Mime::Type::InvalidMimeType"
     ]
-
-    config.send_default_pii = true
-    filter = ActiveSupport::ParameterFilter.new(
-      Rails.application.config.filter_parameters
-    )
-    config.before_send = lambda do |event, _hint|
-      filter.filter(event.to_hash)
-    end
 
     # config.traces_sample_rate = 1.0
     # config.traces_sampler = lambda do |context|
